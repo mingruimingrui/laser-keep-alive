40b896248760d2757fc08eee3533c16cb1b2ce2a
mingruimingrui <mingrui.wang@shopee.com>
2020-04-05 13:36:47 +0800

    remove old encoder

--- laser/encoder.py
+++ laser/encoder.py
@@ -1,4 +1,4 @@
-"""LASER bilstm encoder
+"""LASER LSTM sequence encoder
 """
 
 import torch
@@ -13,101 +13,6 @@ class EncoderOuts(NamedTuple):
 
 
 class Encoder(nn.Module):
-    def __init__(
-        self,
-        num_embeddings,
-        padding_idx,
-        embed_dim=320,
-        hidden_size=512,
-        num_layers=1,
-        bidirectional=False,
-        padding_value=0.
-    ):
-        super().__init__()
-
-        self.num_layers = num_layers
-        self.bidirectional = bidirectional
-        self.hidden_size = hidden_size
-
-        self.padding_idx = padding_idx
-        self.embed_tokens = nn.Embedding(
-            num_embeddings, embed_dim,
-            padding_idx=self.padding_idx
-        )
-
-        self.lstm = nn.LSTM(
-            input_size=embed_dim,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            bidirectional=bidirectional,
-        )
-        self.padding_value = padding_value
-
-        self.output_units = hidden_size
-        if bidirectional:
-            self.output_units *= 2
-
-    def forward(self, src_tokens, src_lengths):
-        bsz, seqlen = src_tokens.size()
-
-        # embed tokens
-        x = self.embed_tokens(src_tokens)
-
-        # B x T x C -> T x B x C
-        x = x.transpose(0, 1)
-
-        # pack embedded source tokens into a PackedSequence
-        packed_x = nn.utils.rnn.pack_padded_sequence(
-            x, src_lengths.data.tolist())
-
-        # apply LSTM
-        if self.bidirectional:
-            state_size = 2 * self.num_layers, bsz, self.hidden_size
-        else:
-            state_size = self.num_layers, bsz, self.hidden_size
-        h0 = x.data.new(*state_size).zero_()
-        c0 = x.data.new(*state_size).zero_()
-        packed_outs, (final_hiddens, final_cells) = \
-            self.lstm(packed_x, (h0, c0))
-
-        # unpack outputs and apply dropout
-        x, _ = nn.utils.rnn.pad_packed_sequence(
-            packed_outs, padding_value=self.padding_value)
-        assert list(x.size()) == [seqlen, bsz, self.output_units]
-
-        if self.bidirectional:
-            def combine_bidir(outs):
-                return torch.cat([
-                    torch.cat([
-                        outs[2 * i],
-                        outs[2 * i + 1]
-                    ], dim=0).view(1, bsz, self.output_units)
-                    for i in range(self.num_layers)
-                ], dim=0)
-
-            final_hiddens = combine_bidir(final_hiddens)
-            final_cells = combine_bidir(final_cells)
-
-        encoder_padding_mask = src_tokens.eq(self.padding_idx).t()
-        if not encoder_padding_mask.any():
-            encoder_padding_mask = None
-
-        # Set padded outputs to -inf so they are not selected by max-pooling
-        padding_mask = src_tokens.eq(self.padding_idx).t().unsqueeze(-1)
-        if padding_mask.any():
-            x = x.float().masked_fill_(padding_mask, float('-inf')).type_as(x)
-
-        # Build the sentence embedding by max-pooling over the encoder outputs
-        sentemb = x.max(dim=0)[0]
-
-        return EncoderOuts(
-            sentemb=sentemb,
-            encoder_out=(x, final_hiddens, final_cells),
-            encoder_padding_mask=encoder_padding_mask
-        )
-
-
-class EncoderNew(nn.Module):
 
     def __init__(
         self,
@@ -209,9 +114,8 @@ class EncoderNew(nn.Module):
 
         encoder_out = None
         if return_encoder_out:
-            if self.bidirectional:
-                final_hiddens = self._combine_outs(final_hiddens)
-                final_cells = self._combine_outs(final_cells)
+            final_hiddens = self._combine_outs(final_hiddens)
+            final_cells = self._combine_outs(final_cells)
             encoder_out = (x, final_hiddens, final_cells)
 
         encoder_padding_mask = None
@@ -238,6 +142,8 @@ class EncoderNew(nn.Module):
         #   -> [num_layers, num_dir, bsz, hidden_size]
         #   -> [num_layers, bsz, num_dir, hidden_size]
         #   -> [num_layers, bsz, num_dir * hidden_size]
+        if self.num_directions == 1:
+            return x
         x = x.reshape(
             self.num_layers,
             self.num_directions,
