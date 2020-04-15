71a795602aaee55e7510e72023385e4c5621b3d8
mingruimingrui <mingruimingrui@hotmail.com>
2020-04-16 00:05:35 +0800

    Fixed LICENSE

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+# Copyright (c) 2020 mingruimingrui
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Adapted directly from
+# https://github.com/facebookresearch/LASER/blob/master/install_models.sh
+
 s3="https://dl.fbaipublicfiles.com/laser/models"
 filenames=(
     "bilstm.eparl21.2018-11-19.pt"
@@ -12,6 +22,8 @@ filenames=(
 mkdir -p models
 for filename in ${filenames[@]}; do
     if [ ! -f models/$filename ]; then
+        echo "Downloading $filename"
         curl -o models/$filename $s3/$filename
     fi
 done
+echo "Download complete"
